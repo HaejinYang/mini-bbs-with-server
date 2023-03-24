1e7517ea425aadb98f2e6101f99d8a04a3705502
haejin <haejin.yang91@gmail.com>
2023-03-24 22:06:26 +0900

    feat: cors 설정

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,12 +4,18 @@ import dotenv from 'dotenv';
 import dotenvExpand from 'dotenv-expand';
 import postRouter from "./routes/post/post";
 import commentRouter from './routes/comment/comment';
+import cors from 'cors';
 
 dotenvExpand.expand(dotenv.config());
 
 const app = express();
 const port: number = Number.parseInt(process.env.SERVER_PORT!);
 
+const corsOption = {
+    origin: 'http://127.0.0.1:3000'
+};
+
+app.use(cors(corsOption));
 app.use(express.json());
 
 app.use('/api', postRouter);
